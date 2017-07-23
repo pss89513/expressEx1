c72022ff17fa6a90880f5b56330c2502a30dcb81
=parksungsu <pss89513@gmail.com>
2017-07-23 17:57:56 +0900

    mogoose CURD 구현 (TDD 활용)

--- routes/todos.js
+++ routes/todos.js
@@ -5,29 +5,73 @@ const todoRoute = express.Router();
 
 todoRoute.get('/todos', (req, res) => {
 
-    // Todo.find(undefined, { _id: 1, todo: 1, isDone: 1 }, (err, todos) => {
-    //     if (err) {
-    //         return res.status(500).send({ err: 'db error' });
-    //     }
-    //     res.send(todos);
-    // });    
-
     Todo.find({ __v: false })
         .then(todos => {
-            return res.send(todos);
+            res.send(todos);
         })
         .catch(err => {
-            res.status(500).send( {err: 'db err'} );
+            res.status(500).send({ err: 'db err' });
         });
 });
 
 todoRoute.get('/todo/:id', (req, res) => {
-    Todo.findById(req.params.id, {__v: false} ,(err, todo) => {
-        if(err) {
-            return res.status(500).send({ err: 'db error' });
-        }
-        res.send(todo);
-    });
+
+    Todo.findById(req.params.id, { __v: false })
+        .then(todo => {
+            res.send(todo);
+        })
+        .catch(err => {
+            res.status(500).send({ err: 'db error' });
+        });
+
+});
+
+todoRoute.post('/add', (req, res) => {
+    let todo = new Todo();
+    todo.todo = req.body.todo;
+    todo.save()
+        .then(() => {
+            res.status(200).send({ result: true });
+        })
+        .catch(err => {
+            res.status(500).send({ result: false });
+        });
+});
+
+todoRoute.put('/check/:id', (req, res) => {
+    Todo.findById(req.params.id).exec()
+        .then(todo => {
+            if (todo) {
+                todo.isDone = !todo.isDone;
+                return todo.save();
+            }
+        })
+        .then(todo => {
+            if (!todo) {
+                res.status(404).send({ result: false });
+            }
+            else {
+                res.status(200).send({ result: true });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ result: false });
+        });
+});
+
+todoRoute.delete('/remove/:id', (req, res) => {
+    Todo.findByIdAndRemove(req.params.id)
+        .then((todo) => {
+            if (!todo) {
+                res.status(404).send({ result: false });
+            } else {
+                res.status(200).send({ result: true });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({ result: false });
+        });
 });
 
 export default todoRoute;
--- tests/routes/todos.test.js
+++ tests/routes/todos.test.js
@@ -7,11 +7,13 @@ import Todo from '../../models/todo';
 const todos = [
     {
         _id: new mongooose.Types.ObjectId(),
-        todo: "밥 먹기"
+        todo: "밥 먹기",
+        isDone: false
     },
     {
         _id: new mongooose.Types.ObjectId(),
-        todo: "휴가 계획 만들기"
+        todo: "휴가 계획 만들기",
+        isDone: false
     }
 ];
 beforeEach((done) => {
@@ -37,7 +39,6 @@ describe('GET /todos', () => {
                     done(err);
                     return;
                 }
-                console.log(res.body);
                 expect(res.body.length).to.equal(2);
                 done();
             });
@@ -54,9 +55,89 @@ describe('GET /todos/:id', () => {
             .end((err, res) => {
                 if(err)
                     done(err);
-                console.log(res.body);
                 expect(res.body.todo).equal(todos[0].todo);
                 done();
             });
     });
+});
+
+describe('POST /add', () => {
+
+    it('새로운 todo를 입력하기', (done) => {
+        const newTodo = "양파즙 먹기";
+        request(app)
+            .post('/add')
+            .send({
+                todo: newTodo
+            })
+            .expect(200)
+            .end((err, res) => {
+                if(err)
+                    done(err);
+                expect(res.body.result).equal(true);
+
+                Todo.findOne({todo:newTodo})
+                    .then(todo => {
+                        expect(todo.todo).to.equals(newTodo);                        
+                    })
+                    .catch(err => {
+                        done(err);
+                    })
+
+                done();
+            });
+    });
+});
+
+describe('PUT /check/:id', () => {
+    it('todo 완료 체크 테스트', (done) => {
+        request(app)
+            .put(`/check/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .end((err, res) =>{
+                expect(res.body.result).equal(true);
+                Todo.findById(todos[0]._id.toHexString())
+                    .then(todo => {
+                        expect(todo.isDone).equal(!todos[0].isDone);
+                    })
+                    .catch(err => {
+                        done(err);
+                    })
+                done();
+            });
+    });
+    
+    it('todo 완료 체크 시 없는 id를 조회할 경우 404 리턴', (done) => {
+        const tmpId = new mongooose.Types.ObjectId();
+        request(app)
+            .put(`/check/${tmpId}`)
+            .expect(404)
+            .end(done);
+    });
+});
+
+describe('DELETE /remove/:id', () => {
+    it('todo 삭제 테스트', (done) => {
+        request(app)
+            .delete(`/remove/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .end((err, res) => {
+                Todo.findById(todos[0]._id.toHexString())
+                    .then(todo => {
+                        expect(todo).to.be.null;
+                        done();
+                    })
+                    .catch(err => {
+                        done(err);
+                    });
+            });
+    });
+    
+    it('todo 삭제 시 없는 id를 조회할 경우 404 리턴', (done) => {
+        const tmpId = new mongooose.Types.ObjectId();
+        request(app)
+            .delete(`/remove/${tmpId}`)
+            .expect(404)
+            .end(done);
+    })
 });
